a8393142ecb0cf4a920aec50e5feafb9e146366f
acornriver <kmicky0806@gmail.com>
2024-12-10 15:51:16 +0900

    라인 길이 줄이기

--- Line.js
+++ Line.js
@@ -12,7 +12,7 @@ class Line extends Particle {
     translate(this.position.x, this.position.y);
     let theta = map(this.position.x, 0, width, TWO_PI*-2, TWO_PI * 2);
     rotate(theta);
-    line(0, 0, 30, 30);
+    line(0, 0, 10, 10);
     pop();
   }
 }
