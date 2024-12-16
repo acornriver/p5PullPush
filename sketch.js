a58dfa8034791ff2f2baf916f5bad62070796dc7
acornriver <kmicky0806@gmail.com>
2024-12-16 19:26:57 +0900

    캔버스 사이즈 좀 키우기

--- sketch.js
+++ sketch.js
@@ -3,8 +3,12 @@
 let particleSystem;
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(800, 800);
   particleSystem = new ParticleSystem(createVector(width / 2, height/2));
+
+  strokeWeight(4);
+
+  line(0, 0, width, 0);
 }
 
 function draw() {
